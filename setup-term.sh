174b9810c252923fc64b3629dd2e4942fd27683c
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2022-12-03 12:35:36 +0300

    added .nvmrc and .nanorc

--- setup-term.sh
+++ setup-term.sh
@@ -4,10 +4,14 @@ PFTERMDIR=$HOME/.pixelfiles/pixelfiles-term
 
 rm -rf "$HOME"/.zsh
 rm -rf "$HOME"/.zshrc
+rm -rf "$HOME"/.nvmrc
+rm -rf "$HOME"/.nanorc
 rm -rf "$HOME"/.gitconfig
 rm -rf "$HOME"/.Xresources
 
 ln -s "$PFTERMDIR"/.zsh "$HOME"/.zsh
 ln -s "$PFTERMDIR"/.zshrc "$HOME"/.zshrc
+ln -s "$PFTERMDIR"/.nvmrc "$HOME"/.nvmrc
+ln -s "$PFTERMDIR"/.nanorc "$HOME"/.nanorc
 ln -s "$PFTERMDIR"/.gitconfig "$HOME"/.gitconfig
 ln -s "$PFTERMDIR"/.Xresources "$HOME"/.Xresources
